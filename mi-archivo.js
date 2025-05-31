d08e1924f05911280d3026e9c8efdfe55d14a037
zacalfaro97 <37884827+zacalfaro97@users.noreply.github.com>
2025-05-31 15:55:54 -0600

    Update mi-archivo.js
    prueba desde la nueva branch

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,3 +1,4 @@
 alert("hola mundo_editado");
 console.log("cambios en el archivo");
-<h1>Modificado desde GitHub Desktop</h1>
+<h1>Modificado desde GitHub Desktop</h1>    
+console.log("cambios en el archivo desde GitHub Desktop branch prueba");
